a085558ba4b1812e88c98691bdfdbfb9c717b280
Kahon7586 <kahon7586@gmail.com>
2024-01-28 16:09:31 +0800

    WIP: PageHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
+import PageHeader from "./Layouts/PageHeader";
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex max-h-screen flex-col">
+      <PageHeader />
+      <div>2</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
