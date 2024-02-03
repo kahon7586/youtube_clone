5ae3a3f469abcc585baef1a692725dc57cf7fd29
Kahon7586 <kahon7586@gmail.com>
2024-02-03 11:54:20 +0800

    add: structure for video grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import CategoryPills from "./Components/CategoryPills";
-import { categories } from "./Data/home";
+import { categories, videos } from "./Data/home";
 import PageHeader from "./Layouts/PageHeader";
+import VideoGridItem from "./Components/VideoGridItem";
 
 function App() {
   const [selectedCategory, setSelectCategory] = useState(categories[0]);
@@ -19,6 +20,14 @@ function App() {
               selectedCategory={selectedCategory}
               onSelect={setSelectCategory}
             />
+          </div>
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+            {/* auto-fill: fill with maximum number according to col width;
+             minmax: 300px for min width and 1fr for max width */}
+            {videos.map((video) => {
+              return <VideoGridItem key={video.id} {...video} />;
+              // notice that we can automatically fit attributes with spread syntax and key-value pairs
+            })}
           </div>
         </div>
       </div>
