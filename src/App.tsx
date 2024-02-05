3306d893168c0086d022677108c1e9b2567e85a6
Kahon7586 <kahon7586@gmail.com>
2024-02-05 23:26:38 +0800

    add: SidebarContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,37 @@ import { categories, videos } from "./Data/home";
 import PageHeader from "./Layouts/PageHeader";
 import VideoGridItem from "./Components/VideoGridItem";
 import Sidebar from "./Layouts/Sidebar";
+import SidebarContextProvider from "./Context/SidebarContextProvider";
 
 function App() {
   const [selectedCategory, setSelectCategory] = useState(categories[0]);
 
   return (
-    <div className="flex max-h-screen flex-col">
-      <PageHeader />
-      <div className="flex-grow-1 grid grid-cols-[auto,1fr] overflow-auto">
-        <Sidebar />
-        <div className="overflow-x-hidden px-8 pb-4">
-          <div className="sticky top-0 z-10 bg-white pb-4">
-            <CategoryPills
-              categories={categories}
-              selectedCategory={selectedCategory}
-              onSelect={setSelectCategory}
-            />
-          </div>
-          <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
-            {/* auto-fill: fill with maximum number according to col width;
-             minmax: 300px for min width and 1fr for max width */}
-            {videos.map((video) => {
-              return <VideoGridItem key={video.id} {...video} />;
-              // notice that we can automatically fit attributes with spread syntax and key-value pairs
-            })}
+    <SidebarContextProvider>
+      <div className="flex max-h-screen flex-col">
+        <PageHeader />
+        <div className="flex-grow-1 grid grid-cols-[auto,1fr] overflow-y-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 z-10 bg-white pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectCategory}
+              />
+            </div>
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+              {/* auto-fill: fill with maximum number according to col width;
+              minmax: 300px for min width and 1fr for max width */}
+              {videos.map((video) => {
+                return <VideoGridItem key={video.id} {...video} />;
+                // notice that we can automatically fit attributes with spread syntax and key-value pairs
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </SidebarContextProvider>
   );
 }
 
